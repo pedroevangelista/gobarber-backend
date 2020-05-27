5609ba850ba4b4e0668769753b46b8159db157c1
Pedro Evangelista <pedroevangelista@outlook.com>
2020-05-27 00:19:21 -0300

    Provider's schedule

---
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import ListProvidersAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
+
+export default class ProviderAppointmentsController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const provider_id = request.user.id;
+    const { day, month, year } = request.body;
+
+    const listProviderAppointments = container.resolve(
+      ListProvidersAppointmentsService,
+    );
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id,
+      day,
+      month,
+      year,
+    });
+
+    return response.json(appointments);
+  }
+}
