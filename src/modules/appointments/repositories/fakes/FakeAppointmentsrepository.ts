8c8a26651576e9c4a054dd7580b4a355f3161ba4
Pedro Evangelista <pedroevangelista@outlook.com>
2020-06-06 01:18:59 -0300

    Appointments fixes

--- src/modules/appointments/repositories/fakes/FakeAppointmentsrepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsrepository.ts
@@ -9,9 +9,14 @@ import IFindAllInDayFromProviderDTO from '@modules/appointments/dtos/IFindAllInD
 class FakeAppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
 
-  public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointments = this.appointments.find(appointment =>
-      isEqual(appointment.date, date),
+  public async findByDate(
+    date: Date,
+    provider_id: string,
+  ): Promise<Appointment | undefined> {
+    const findAppointments = this.appointments.find(
+      appointment =>
+        isEqual(appointment.date, date) &&
+        appointment.provider_id === provider_id,
     );
 
     return findAppointments;
